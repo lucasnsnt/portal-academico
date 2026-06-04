99033a872f1065cd3dd9e8d42b3e9d24a0939257
Lucas Santos <lucasnsntcontato@outlook.com>
2026-06-04 19:15:37 -0300

    Initial commit

---
+++ src/main/java/com/github/lucasnsnt/portal_academico/PortalAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasnsnt.portal_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasnsnt/portal_academico/PortalAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasnsnt.portal_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
